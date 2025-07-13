beadb790922c1c4a8c4fe7b36c438886789b2f98
Jong-Min <codejomo99@gmail.com>
2025-07-13 18:41:47 +0900

    feat: 데이터 추가, 데이터 집계 api

---
+++ backend/drug/src/main/java/com/side/drug/controller/OrganizeController.java
@@ -0,0 +1,35 @@
+package com.side.drug.controller;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.side.drug.config.DataInitializer;
+import com.side.drug.service.DrugOrganizeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class OrganizeController {
+
+	private final DrugOrganizeService organizeService;
+	private final DataInitializer dataInitializer;
+
+	// 수동 데이터 추가
+	@PostMapping("import")
+	public ResponseEntity<String> importData() throws IOException {
+		dataInitializer.loadExcel();
+
+		return ResponseEntity.ok("엑셀 데이터 DB로 import 완료");
+	}
+
+
+	@PostMapping("/organize")
+	public ResponseEntity<String> runOrganize() {
+		organizeService.organize();
+		return ResponseEntity.ok("Organizing completed!");
+	}
+}
